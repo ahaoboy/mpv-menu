a75f6dfca24966acc11ef0b7ef689a73653be462
ahaoboy <504595380@qq.com>
2025-12-31 16:38:38 +0800

    dialog

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ curl -O -L https://github.com/tsl0922/mpv-menu-plugin/releases/latest/download/m
 unzip -o menu.zip -d ./
 mv ./menu/menu.dll ./
 mv ./menu/dyn_menu.lua ./mpv-menu.lua
-mv ./menu/dialog.lua ./mpv-dialog.lua
+# mv ./menu/dialog.lua ./mpv-dialog.lua
 rm -rf ./menu
